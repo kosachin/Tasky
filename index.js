b84ef92c2a3be7cf661e1aec365998ad40ca0d54
sachin <kosachin313@gmail.com>
2021-06-24 12:19:29 +0530

    add localstorage

---
+++ index.js
@@ -0,0 +1,69 @@
+const taskContainer = document.querySelector(".task__container")
+console.log(taskContainer)
+
+const globalStore = [];
+
+const generateNewCard = (taskData)=>`
+<div class="col-md-6 col-lg-4 mb-3" id=${taskData.id}>
+                <div class="card ">
+                    <div class="card-header text-end gap-2">
+                        <button type="button" class="btn btn-outline-success"><i
+                                class="fas fa-pencil-alt"></i></button>
+                        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash"></i></button>
+                    </div>
+                    <img src=${taskData.imageUrl} class="card-img-top" alt="card-img">
+                    <div class="card-body">
+                        <h5 class="card-title">${taskData.taskTitle}</h5>
+                        <p class="card-text">${taskData.taskDescription}
+                        </p>
+                        <a href="#" class="btn btn-primary">${taskData.taskType}</a>
+                    </div>
+                    <div class="card-footer float-end">
+                        <button type="button" class="btn btn-outline-primary">Open Task</button>
+                    </div>
+                </div>
+            </div>
+`
+// when we reload page data is tored in localStorage but their cards were not showing
+const loadInitialCardData = () => {
+    // localStorage to get tasky card data
+    const getCardData = localStorage.getItem("tasky");
+
+    // convert from string to normal object
+    const {cards} = JSON.parse(getCardData);
+
+    // loop over those array of task object to create HTML card, inject it to DOM
+    cards.map((cardObject) => {
+    
+        taskContainer.insertAdjacentHTML("beforeend",generateNewCard(cardObject));
+        
+        // update our globalStore
+        globalStore.push(cardObject);
+         
+    })
+
+}
+
+const saveChanges = () => {
+    const taskData = {
+        id: `${Date.now()}`,
+        imageUrl : document.getElementById("imageUrl").value,
+        taskTitle : document.getElementById("taskTitle").value,
+        taskType : document.getElementById("taskType").value,
+        taskDescription : document.getElementById("taskDescription").value,
+    };
+    
+    // To create html code(generateNewCard) from object(taskData)
+    // And to add next to the card
+    taskContainer.insertAdjacentHTML("beforeend",generateNewCard(taskData));
+    
+    // To push/add every new card object(taskData) into the array(globalStore)
+    globalStore.push(taskData);
+
+    
+    // Adding objects from array to localStorage 
+    // ID is given to identify data
+    // To convert JS object to string stringify used since it aspects object key name cards is created with value as array of object
+    localStorage.setItem("tasky", JSON.stringify({cards:globalStore}));
+
+};
